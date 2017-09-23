25b44ad97e43717363593e2c0c0b2ae9f136b5b3
小t <737005431@qq.com>
2017-09-24 01:06:26 +0800

    test

--- index.py
+++ index.py
@@ -1,31 +1,57 @@
 #-*- coding:utf-8 -*-
+from bottle import *
+import hashlib
+import xml.etree.ElementTree as ET
+import urllib2
+import json
+import time
 
-def app(environ, start_response):
-    status = '200 OK'
-    headers = [('Content-type', 'text/html')]
-    start_response(status, headers)
-    body_str = """
-    <html>
-        <head>
-            <meta charset="utf-8">
-            <title>Welcome to BAE</title>
-        </head>
-        <body>
-            <h4>Welcome to Baidu App Engine!</h4>
-            <h2>欢迎开启BAE之旅！</h2>
-            <br>
-            <br>
-            <ul>以下链接可能对您有用：</ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/QuickGuide.html" target="_blank">入门指南</a></ul>
-            <ul><a href="http://developer.baidu.com/forum/topic/list?boardId=66" target="_blank">论坛</a></ul>
-            <ul><a href="http://www.baeblog.com/" target="_blank">技术博客</a></ul>
-            <ul><a href="http://weibo.com/baiduappengine" target="_blank">微博</a></ul>
-            <ul><a href="https://bce.baidu.com/doc/BAE/FAQ.html" target="_blank">常见问题</a></ul>
-        </body>
-    </html>
-    """
-    body=[body_str]
-    return body
+app = Bottle()
 
-from bae.core.wsgi import WSGIApplication
-application = WSGIApplication(app)
+
+@app.get('/')
+def checkSignature():
+    token = "tonghuanmingdeweixin"
+    signature = request.GET.get('signature', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    echostr = request.GET.get('echostr', None)
+    tmpList = [token, timestamp, nonce]
+    tmpList.sort()
+    tmpstr = "%s%s%s" % tuple(tmpList)
+    hashstr = hashlib.sha1(tmpstr).hexdigest()
+    if hashstr == signature:
+        return echostr
+    else:
+        return False
+
+
+def parse_msg():
+    recvmsg = request.body.read()
+    root = ET.fromstring(recvmsg)
+    msg = {}
+    for child in root:
+        msg[child.tag] = child.text
+    return msg
+
+
+@app.post("/")
+def response_msg():
+    msg = parse_msg()
+    echostr = """<xml> 
+    <ToUserName><![CDATA[%s]]></ToUserName> 
+    <FromUserName><![CDATA[%s]]></FromUserName> 
+    <CreateTime>%s</CreateTime> 
+    <MsgType><![CDATA[%s]]></MsgType> 
+    <Content><![CDATA[%s]]></Content> 
+    </xml>""" % (msg['FromUserName'], msg['ToUserName'], time.time(), "text", msg['Content'])
+    return echostr
+
+
+if __name__ == "__main__":
+    debug(True)
+    run(app, host='127.0.0.1', port=8080, reloader=True)
+
+else:
+    from bae.core.wsgi import WSGIApplication
+    application = WSGIApplication(app)
