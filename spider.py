e588de743d0e8e3fe8c09aa6098e2bccc9b1b58f
小t <737005431@qq.com>
2017-09-24 13:06:20 +0800

    test

--- spider.py
+++ spider.py
@@ -1,53 +1,38 @@
-# -*- coding: utf-8 -*-
-from bs4 import BeautifulSoup
-import requests
-import time
+#-*- coding:utf-8 -*-
+
 import pymongo
-
-db_name = 'dwADfZdbrNknnSLAmPxt'
-con = pymongo.MongoClient('mongo.duapp.com', 8908)
-db = con['db_name']
-api_key = '48085b6296ac48acb99e6ff71e863630'
-secret_key = 'dbd3fcb2c6034b4986364603334d6ffe'
-db.authenticate(api_key, secret_key)
-coupons = db['coupons']
-
-header = {
-    'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1'
-}
-url = 'https://m.lapin365.com/index/GetHomeListAjax'
+from pymongo import MongoClient
 
 
-def get_goods(url, page=1, data=None):
-    payload = {'limit': 10, 'pageIndex': page, 'clienttype': 0}
-    wb_data = requests.post(url, data=payload, headers=header)
-    soup = BeautifulSoup(wb_data.text, 'lxml')
-    titles = soup.select('li > a.couponlink.pic-wrap')
-    links = soup.select('li > a.couponlink.pic-wrap')
-    imgs = soup.select('li > a.couponlink.pic-wrap > img.lazy')
-    originprices = soup.select('div.salesinfo > del.origin-price')
-    discountprices = soup.select('span.aftercoupon > span')
+def test_mongo():
+    # 连接MongoDB服务
+    # 从管理控制台获取host, port, db_name, api_key, secret_key
+    con = MongoClient("mongo.duapp.com", 8908)
+    db_name = "dwADfZdbrNknnSLAmPxt"  # 数据库名称
+    db = con[db_name]
+    api_key = "48085b6296ac48acb99e6ff71e863630"  # 用户AK
+    secret_key = "dbd3fcb2c6034b4986364603334d6ffe"  # 用户SK
+    db.authenticate(api_key, secret_key)
 
-    if data is None:
-        for title, link, img, originprice, discountprice in zip(titles, links, imgs, originprices, discountprices):
-            data = {
-                'title': title.get('title'),
-                'link': link.get('data-couponbuylink'),
-                'img': img.get('data-original'),
-                'originprice': originprice.get_text()[1:-1],
-                'discountprice': discountprice.get_text(),
-            }
-            coupons.insert_one(data)
-            print data
-    return data
+    # 插入数据到集合test
+    collection_name = 'test'
+    db[collection_name].insert({"id": 10, 'value': "test test"})
+
+    # 查询集合test
+    cursor = db[collection_name].find()
+    con.close()
+    return "select collection test %s" % str(cursor[0])
 
 
-page_num = 1
-while(1):
-    res = get_goods(url, page_num)
-    if res is None:
-        con.close()
-        break
-    else:
-        page_num = page_num + 1
-    time.sleep(1)
+def app(environ, start_response):
+    status = '200 OK'
+    headers = [('Content-type', 'text/html')]
+    start_response(status, headers)
+    try:
+        return test_mongo()
+    except Exception as e:
+        return "exception"
+
+
+from bae.core.wsgi import WSGIApplication
+application = WSGIApplication(app)
