1a2e732a280ab0fc5119e151d86e9b9f4fb91cb1
小t <737005431@qq.com>
2017-09-24 12:45:10 +0800

    test

--- index.py
+++ index.py
@@ -15,10 +15,10 @@ app = Bottle()
 @app.get('/')
 def checkSignature():
     token = "tonghuanmingdeweixin"
-    signature = app.request.GET.get('signature', None)
-    timestamp = app.request.GET.get('timestamp', None)
-    nonce = app.request.GET.get('nonce', None)
-    echostr = app.request.GET.get('echostr', None)
+    signature = request.GET.get('signature', None)
+    timestamp = request.GET.get('timestamp', None)
+    nonce = request.GET.get('nonce', None)
+    echostr = request.GET.get('echostr', None)
     tmpList = [token, timestamp, nonce]
     tmpList.sort()
     tmpstr = "%s%s%s" % tuple(tmpList)
@@ -30,7 +30,7 @@ def checkSignature():
 
 
 def parse_msg():
-    recvmsg = app.request.body.read()
+    recvmsg = request.body.read()
     root = ET.fromstring(recvmsg)
     msg = {}
     for child in root:
@@ -40,21 +40,21 @@ def parse_msg():
 
 @app.post("/")
 def response_msg():
-    # con = pymongo.MongoClient('mongo.duapp.com', 8908)
-    # db = con['db_name']
-    # db.authenticate(api_key, secret_key)
-    # coupons = db['coupons']
+    con = pymongo.MongoClient('mongo.duapp.com', 8908)
+    db = con['db_name']
+    db.authenticate(api_key, secret_key)
+    coupons = db['coupons']
     msg = parse_msg()
-    # result = coupons.find(
-    #     {"title": {"$regex": "%s" % msg['Content']}}, {'_id': 0}).limit(5)
-    # con.close()
+    result = coupons.find(
+        {"title": {"$regex": "%s" % msg['Content']}}, {'_id': 0}).limit(5)
+    con.close()
     echostr = """<xml>
     <ToUserName><![CDATA[%s]]></ToUserName>
     <FromUserName><![CDATA[%s]]></FromUserName>
     <CreateTime>%s</CreateTime>
     <MsgType><![CDATA[%s]]></MsgType>
     <Content><![CDATA[%s]]></Content>
-    </xml>""" % (msg['FromUserName'], msg['ToUserName'], time.time(), 'text', msg['Content'])
+    </xml>""" % (msg['FromUserName'], msg['ToUserName'], time.time(), 'text', result if result is not None else '没有结果')
     return echostr
 
 
