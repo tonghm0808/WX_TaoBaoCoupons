3610ad75baadee3c34143d170cf72696244c776d
小t <737005431@qq.com>
2017-09-24 11:45:20 +0800

    test

--- index.py
+++ index.py
@@ -1,21 +1,23 @@
 #-*- coding:utf-8 -*-
-from bottle import *
+import bottle
 import hashlib
 import xml.etree.ElementTree as ET
-import urllib2
-import json
 import time
+import pymongo
 
-app = Bottle()
+db_name = 'dwADfZdbrNknnSLAmPxt'
+api_key = '48085b6296ac48acb99e6ff71e863630'
+secret_key = 'dbd3fcb2c6034b4986364603334d6ffe'
+app = bottle()
 
 
 @app.get('/')
 def checkSignature():
     token = "tonghuanmingdeweixin"
-    signature = request.GET.get('signature', None)
-    timestamp = request.GET.get('timestamp', None)
-    nonce = request.GET.get('nonce', None)
-    echostr = request.GET.get('echostr', None)
+    signature = app.request.GET.get('signature', None)
+    timestamp = app.request.GET.get('timestamp', None)
+    nonce = app.request.GET.get('nonce', None)
+    echostr = app.request.GET.get('echostr', None)
     tmpList = [token, timestamp, nonce]
     tmpList.sort()
     tmpstr = "%s%s%s" % tuple(tmpList)
@@ -27,7 +29,7 @@ def checkSignature():
 
 
 def parse_msg():
-    recvmsg = request.body.read()
+    recvmsg = app.request.body.read()
     root = ET.fromstring(recvmsg)
     msg = {}
     for child in root:
@@ -37,20 +39,27 @@ def parse_msg():
 
 @app.post("/")
 def response_msg():
+    con = pymongo.MongoClient('mongo.duapp.com', 8908)
+    db = con['db_name']
+    db.authenticate(api_key, secret_key)
+    coupons = db['coupons']
     msg = parse_msg()
+    result = db[coupons].find(
+        {"title": {"$regex": "%s" % msg['Content']}}, {'_id': 0}).limit(5)
+    con.close()
     echostr = """<xml> 
     <ToUserName><![CDATA[%s]]></ToUserName> 
     <FromUserName><![CDATA[%s]]></FromUserName> 
     <CreateTime>%s</CreateTime> 
     <MsgType><![CDATA[%s]]></MsgType> 
     <Content><![CDATA[%s]]></Content> 
-    </xml>""" % (msg['FromUserName'], msg['ToUserName'], time.time(), "text", msg['Content'])
+    </xml>""" % (msg['FromUserName'], msg['ToUserName'], time.time(), "text", result)
     return echostr
 
 
 if __name__ == "__main__":
-    debug(True)
-    run(app, host='127.0.0.1', port=8080, reloader=True)
+    app.debug(True)
+    app.run(host='127.0.0.1', port=8080, reloader=True)
 
 else:
     from bae.core.wsgi import WSGIApplication
