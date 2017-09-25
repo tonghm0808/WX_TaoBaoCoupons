72814e847bd8b8bf96675256b9251b7d75b5b333
小t <737005431@qq.com>
2017-09-25 19:53:11 +0800

    test

--- index.py
+++ index.py
@@ -45,7 +45,7 @@ def search_db(temp=None):
     db.authenticate(api_key, secret_key)
     result = []
     for x in db['coupons'].find({"title": {"$regex": temp}}):
-        result.append(x)
+        result.append(repr(x))
     return result
 
 
@@ -63,18 +63,18 @@ def response_msg():
     if len(get_info):
         echostr = textTpl % (msg['FromUserName'],
                              msg['ToUserName'], str(
-                                 int(time.time())), msg['MsgType'], get_info[0].decode("unicode-escape")
+                                 int(time.time())), msg['MsgType'], get_info[0])
                              )
     else:
-        echostr = textTpl % (msg['FromUserName'],
+        echostr=textTpl % (msg['FromUserName'],
                              msg['ToUserName'], str(int(time.time())), msg['MsgType'], '没有搜到结果')
     return echostr
 
 
 if __name__ == '__main__':
     debug(True)
-    run(app, host='127.0.0.1', port=8080, reloader=True)
+    run(app, host = '127.0.0.1', port = 8080, reloader = True)
 
 else:
     from bae.core.wsgi import WSGIApplication
-    application = WSGIApplication(app)
+    application=WSGIApplication(app)
