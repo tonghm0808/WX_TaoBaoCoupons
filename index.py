655f08b1a78ee200957c6108249866fb17a397f6
小t <737005431@qq.com>
2017-10-01 21:44:07 +0800

    test

--- index.py
+++ index.py
@@ -116,7 +116,10 @@ def response_msg():
                              msg['ToUserName'],
                              str(int(time.time())),
                              msg['MsgType'],
-                             '没有搜到结果，请换个关键字搜索！多个关键字之间请用空格分开！\n例如：\n  苹果 数据线\n  家用 吸尘器')
+                             '没有搜到结果，请换个关键字搜索！多个关键字之间请用空格分开！\
+                             \n例如：\
+                             \n    苹果 数据线\
+                             \n    家用 吸尘器')
     return echostr
 
 
