df4fb22993aa8423d075fcb832c99ff461945c32
小t <737005431@qq.com>
2017-10-01 11:47:25 +0800

    test

--- index.py
+++ index.py
@@ -44,7 +44,7 @@ def search_db(temp=None):
     db = con[db_name]
     db.authenticate(api_key, secret_key)
     result = []
-    for x in db['coupons'].find({"title": {"$regex": temp}}).limit(8).sort('biz30Day', -1):
+    for x in db['coupons'].find({'title': {'$regex': temp}}).limit(8).sort('biz30Day', -1):
         result.append(x)
     return result
 
